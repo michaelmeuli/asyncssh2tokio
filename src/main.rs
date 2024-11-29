d92e447ed6064cbc30bd765c20c1ee3a71cda7ea
Michael Meuli <mmeuli@imm.uzh.ch>
2024-11-29 14:40:07 +0100

    create_tasks

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use async_ssh2_tokio::client::{AuthMethod, Client, ServerCheckMethod};
 use directories_next::UserDirs;
+use std::collections::HashMap;
 
 const REMOTE_RAW_DIR: &str = "/shares/sander.imm.uzh/MM/PRJEB57919/raw";
 
@@ -63,10 +64,64 @@ async fn main() -> Result<(), async_ssh2_tokio::Error> {
         println!("{}", file_name);
     }
 
+    let tasks = create_tasks(file_names);
+    for task in tasks {
+        println!("{}", task.sample);
+        println!("{}", task.read1);
+        println!("{}", task.read2);
+        println!("{}", task.is_checked);
+    }
+
     Ok(())
 }
 
 
+
+
+struct Task {
+    sample: String,
+    read1: String,
+    read2: String,
+    is_checked: bool,
+}
+
+fn create_tasks(file_names: Vec<&str>) -> Vec<Task> {
+    let mut tasks = Vec::new();
+
+    // Group files by sample name
+    let mut grouped_files: HashMap<String, (String, String)> = HashMap::new();
+    for file_name in file_names {
+        if let Some((sample, suffix)) = file_name.split_once('_') {
+            match suffix {
+                "1.fastq.gz" => {
+                    grouped_files
+                        .entry(sample.to_string())
+                        .or_insert_with(|| (String::new(), String::new()))
+                        .0 = file_name.to_string();
+                }
+                "2.fastq.gz" => {
+                    grouped_files
+                        .entry(sample.to_string())
+                        .or_insert_with(|| (String::new(), String::new()))
+                        .1 = file_name.to_string();
+                }
+                _ => {}
+            }
+        }
+    }
+
+    // Construct tasks
+    for (sample, (read1, read2)) in grouped_files {
+        tasks.push(Task {
+            sample,
+            read1,
+            read2,
+            is_checked: false,
+        });
+    }
+
+    tasks
+}
 
 fn ssh_key_path() -> Result<String, String> {
     if let Some(user_dirs) = UserDirs::new() {
